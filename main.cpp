61bd1ee77a4cb34d166c45bfaf8d9b43e5ab6297
t01ha05 <t01haa2005@gmail.com>
2024-10-04 18:47:22 -0700

    Update main.cpp
    ran through code outline/beautifier

--- main.cpp
+++ main.cpp
@@ -1,3 +1,11 @@
+// Title: Movie Review System
+// Description: A program to store and manage movie reviews using linked lists.
+//              Each movie has a linked list for ratings and comments.
+//              Reviews are read from an external file and ratings are random.
+// Author: Talha Ahmed
+// Lab: 19
+// Class: COMSC-210
+
 #include <iostream>
 #include <string>
 #include <vector>
@@ -8,100 +16,108 @@
 
 using namespace std;
 
-//need to create node struc for linked list
+// Node structure for linked list
 struct ReviewNode {
     float rating;
     string comment;
     ReviewNode* next;
 };
 
-//class to store review and ratings
+// Movie class to store reviews and ratings
 class Movie {
-    private:
-        string title;
-        ReviewNode* head; //head pointer for linked list of revciew
+private:
+    string title;
+    ReviewNode* head;  // Head pointer for the linked list of reviews
 
-    public:
-        Movie(const string& title) : title(title), head(nullptr){}
-        //add method to adda review *rating/comment
-        void addReview(float rating, const string& comment){
-            ReviewNode* newNode = new ReviewNode;
-            newNode-> rating = rating;
-            newNode->comment = comment;
-            newNode-> next = head; //add to head of list
-            head = newNode;
-        }
+public:
+    Movie(const string& title) : title(title), head(nullptr) {}
 
-        //method to output review and average rating
-        void outputReviews() const {
-            ReviewNode* temp = head;
-            int count = 1;
-            float sum = 0.0;
-            int reviewCount = 0;
+    // Method to add a review (rating and comment)
+    void addReview(float rating, const string& comment) {
+        ReviewNode* newNode = new ReviewNode;
+        newNode->rating = rating;
+        newNode->comment = comment;
+        newNode->next = head;  // Add to the head of the list
+        head = newNode;
+    }
 
-            cout << "Reviews for \"" <<title << "\":\n";
-            while (temp != nullptr) {
-                cout << " Review #" << count << ": "<< fixed << setprecision(1)
-                    <<temp->rating << "-" << temp-> comment << endl;
+    // Method to output reviews and average rating
+    void outputReviews() const {
+        ReviewNode* temp = head;
+        int count = 1;
+        float sum = 0.0;
+        int reviewCount = 0;
+
+        cout << "Reviews for \"" << title << "\":\n";
+        while (temp != nullptr) {
+            cout << "  > Review #" << count << ": " << fixed << setprecision(1) 
+                 << temp->rating << " - " << temp->comment << endl;
             sum += temp->rating;
             reviewCount++;
-            temp = temp-> next;
+            temp = temp->next;
             count++;
         }
+
         if (reviewCount > 0) {
-            cout << " > Average Rating:" << fixed << setprecision(2)
-                 << sum / reviewCount << "\n\n"
-         else {
-            cout << " > No reviews available.\n\n";
+            cout << "  > Average Rating: " << fixed << setprecision(2) 
+                 << sum / reviewCount << "\n\n";
+        } else {
+            cout << "  > No reviews available.\n\n";
         }
     }
 };
 
-//func to gen random ratings between 1.0 and 5.0(1 dec. place)
-float generateRandomRating(){
-    return static_cast<float>((rand()% 41 +10))/10.0; //random #
+// Function to generate random ratings between 1.0 and 5.0 (1 decimal place)
+float generateRandomRating() {
+    return static_cast<float>((rand() % 41 + 10)) / 10.0;  // Random between 1.0 and 5.0
 }
-//now we need a func to read review comments from an extternal file
+
+// Function to read review comments from an external file
 vector<string> readCommentsFromFile(const string& filename) {
     vector<string> comments;
     ifstream file(filename);
 
-    if(!file) {
-        cerr<< "error opening file:" << filename << endl;
+    if (!file) {
+        cerr << "Error opening file: " << filename << endl;
         return comments;
     }
+
     string line;
-    while (getline(file,line)){
-        if (!line.empty()){
+    while (getline(file, line)) {
+        if (!line.empty()) {
             comments.push_back(line);
         }
     }
+
     file.close();
     return comments;
 }
-int main () {
-    srand(static_cast<unsigned>(time(0))); //to seed random number gen
 
-    //read review comm from file\
-    vector<string> comments = readCommentsFromFile("review.txt");  
-}
+int main() {
+    srand(static_cast<unsigned>(time(0)));  // Seed the random number generator
 
-//now create two movies( i am going to use my favorites)
+    // Read review comments from file (assumes 'reviews.txt' exists with comments)
+    vector<string> comments = readCommentsFromFile("reviews.txt");
+    if (comments.empty()) {
+        cerr << "No comments available to add reviews.\n";
+        return 1;
+    }
 
-Movie movie1("The Shawshank Redemption");
-Movie movie2("The Godfather");
+    // Create two movies
+    Movie movie1("The Shawshank Redemption");
+    Movie movie2("The Godfather");
 
-//reviews to movies, add
-movie1.addReview(generatorRandomRating(), comments[0]);
-movie1.addReview(generatorRandomRating(), comments[1]);
+    // Adding reviews to movie1
+    movie1.addReview(generateRandomRating(), comments[0]);
+    movie1.addReview(generateRandomRating(), comments[1]);
 
-movie2.addReview(generatorRandomRating(), comments[2]);
-movie2.addReview(generatorRandomRating(), comments[3]);
+    // Adding reviews to movie2
+    movie2.addReview(generateRandomRating(), comments[2]);
+    movie2.addReview(generateRandomRating(), comments[3]);
 
-//output review
-movie1.outputReviews();
-movie2.outoutReviews();
+    // Output reviews for each movie
+    movie1.outputReviews();
+    movie2.outputReviews();
 
-return 0;
-
+    return 0;
 }
